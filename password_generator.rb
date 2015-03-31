4276fb405390aedf015e2168fea2cf6f35e32702
Sam Shiffman <sam@samshiffman.com>
2015-03-31 09:53:48 -0700

    bugfix and prettify

--- password_generator.rb
+++ password_generator.rb
@@ -5,30 +5,32 @@
 lowercase = ('a'..'z').to_a
 uppercase = ('A'..'Z').to_a
 numbers = ('0'..'9').to_a
-specials = %w{! @ # $ % ^ & * < > ?}
+specials = %w{! @ # $ % ^ & * < > ? ( ) [ ] { } /}
 
-puts "Welcome to Sam's password generator."
-print "How many characters do you want in your password? "
+print %q{  Welcome to Sam's password generator.
+  How many characters do you want in your password?
+  [The default will be 8 if you just hit ENTER] }
 pw_length = gets.to_i
+pw_length = 8 if pw_length == 0
 
-print "Are any special characters allowed? [Y/n] "
+print "  Are any special characters allowed? [Y/n] "
 specials_allowed = gets.to_s.downcase
 
 if specials_allowed =~ /.?n.?/
   specials = []
 else
-  puts "Sometimes special characters like @ are prohibited."
-  print "Are any special characters prohibited? [Y/n] "
+  puts "  Sometimes special characters like @ are prohibited."
+  print "  Are any special characters prohibited? [Y/n] "
   some_prohibited = gets.to_s.downcase
   if some_prohibited =~ /.?y.?/
-    puts "Type each prohibited character followed by ENTER."
-    puts "Hit ENTER twice in a row when you are finished."
-    prohibited = gets("\n\n").chomp.split
+    puts "  Type each prohibited character."
+    print "  Hit ENTER twice in a row when you are finished.\n  "
+    prohibited = gets("\n\n").chomp.split(//)
     specials -= prohibited
   end
 end
 
 puts "Here is your randomly generated password:"
-puts (lowercase + uppercase + numbers + specials).shuffle[0..pw_length].join
+puts (lowercase + uppercase + numbers + specials).shuffle[0...pw_length].join
 
 
